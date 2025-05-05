22d67d35c050a75d6fd4630cdb2e874f445ac372
apehex <apehex@protonmail.com>
2025-05-05 11:24:43 +0200

    Add a transformer layer operating on patches
    The layer splits the 2D input into patches and applies attention on both spatial axes

---
+++ tests/layers/test_transformer.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+
+import tr1cot.layers.transformer
+
+# PATCHING #####################################################################
+
+class PatchTransformerBlockTest(tf.test.TestCase):
+    def setUp(self):
+        super(PatchTransformerBlockTest, self).setUp()
+        self._null_cases = [
+            {
+                'inputs': tf.ones((2, 16, 16, 8), dtype=tf.float16),
+                'args': {'patch_dim': 2, 'head_num': 2, 'dropout_rate': 0.1, 'epsilon_rate': 1e-6,},
+                'shapes': {'patched': (2, 8, 8, 32), 'merged': (2, 64, 32), 'attended': (2, 64, 32), 'divided': (2, 8, 8, 32), 'unpatched': (2, 16, 16, 8)},
+                'outputs': tf.ones((2, 16, 16, 8), dtype=tf.float32),},
+            {
+                'inputs': tf.ones((2, 4, 16, 8), dtype=tf.float16),
+                'args': {'patch_dim': 2, 'head_num': 2},
+                'shapes': {'patched': (2, 2, 8, 32), 'merged': (2, 16, 32), 'attended': (2, 16, 32), 'divided': (2, 2, 8, 32), 'unpatched': (2, 4, 16, 8)},
+                'outputs': tf.ones((2, 4, 16, 8), dtype=tf.float32),},
+            {
+                'inputs': tf.ones((2, 16, 16, 8), dtype=tf.float16),
+                'args': {'patch_dim': 1, 'head_num': 4},
+                'shapes': {'patched': (2, 16, 16, 8), 'merged': (2, 256, 8), 'attended': (2, 256, 8), 'divided': (2, 16, 16, 8), 'unpatched': (2, 16, 16, 8)},
+                'outputs': tf.ones((2, 16, 16, 8), dtype=tf.float32),},
+            {
+                'inputs': tf.cast([16 * [16 * [8 * [__i + 1]]] for __i in range(2)], dtype=tf.float32),
+                'args': {},
+                'shapes': {'patched': (2, 4, 4, 128), 'merged': (2, 16, 128), 'attended': (2, 16, 128), 'divided': (2, 4, 4, 128), 'unpatched': (2, 16, 16, 8)},
+                'outputs': tf.cast([16 * [16 * [8 * [__i + 1]]] for __i in range(2)], dtype=tf.float32),},]
+
+    def test_shapes_and_dtypes(self):
+        for __case in self._null_cases:
+            __layer = tr1cot.layers.transformer.PatchTransformerBlock(**__case['args'])
+            # end-to-end
+            __outputs = __layer(__case['inputs'], training=False)
+            self.assertEqual(tuple(__case['shapes']['unpatched']), tuple(__outputs.shape))
+            self.assertEqual(__case['outputs'].dtype, __outputs.dtype)
+            # patching space axes
+            __outputs = __layer._patch_space(__case['inputs'])
+            self.assertEqual(tuple(__case['shapes']['patched']), tuple(__outputs.shape))
+            # merging space axes
+            __outputs = __layer._merge_space(__outputs)
+            self.assertEqual(tuple(__case['shapes']['merged']), tuple(__outputs.shape))
+            # attending merged axis
+            __outputs = __layer._attend_space(query=__outputs, key=__outputs, value=__outputs, training=False, use_causal_mask=False)
+            self.assertEqual(tuple(__case['shapes']['attended']), tuple(__outputs.shape))
+            # dividing the space axis
+            __outputs = __layer._split_space(__outputs)
+            self.assertEqual(tuple(__case['shapes']['divided']), tuple(__outputs.shape))
+            # unpatching
+            __outputs = __layer._unpatch_space(__outputs)
+            self.assertEqual(tuple(__case['shapes']['unpatched']), tuple(__outputs.shape))
+            self.assertEqual(__case['outputs'].dtype, __outputs.dtype)
+
+    def test_outputs_equal_inputs_when_constant(self): # outputs = inputs + layer(inputs), the bias is initialized to zero and the norm cancels the product
+        for __case in self._null_cases:
+            __layer = tr1cot.layers.transformer.PatchTransformerBlock(**__case['args'])
+            __outputs = __layer(__case['inputs'], training=False)
+            self.assertAllEqual(__outputs, __case['outputs'])
+
+# DOWN SAMPLING ################################################################
+
+# UP SAMPLING ##################################################################
---
+++ tr1cot/layers/transformer.py
@@ -0,0 +1,96 @@
+import math
+
+import tensorflow as tf
+
+import mlable.blocks.transformer
+import mlable.layers.embedding
+import mlable.layers.shaping
+import mlable.blocks.shaping
+
+# CONSTANTS ####################################################################
+
+# meta
+EPSILON = 1e-5
+DROPOUT = 0.0
+
+# attention blocks
+PATCH_DIM = 0 # P
+HEAD_NUM = 0 # N
+
+# TIME ATTENTION ###############################################################
+
+@tf.keras.utils.register_keras_serializable(package='blocks')
+class PatchTransformerBlock(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        patch_dim: int=PATCH_DIM,
+        head_num: int=HEAD_NUM,
+        dropout_rate: float=DROPOUT,
+        epsilon_rate: float=EPSILON,
+        **kwargs
+    ) -> None:
+        # init
+        super(PatchTransformerBlock, self).__init__(**kwargs)
+        # config
+        self._config = {
+            'patch_dim': patch_dim,
+            'head_num': head_num,
+            'dropout_rate': dropout_rate,
+            'epsilon_rate': epsilon_rate,}
+        # layers
+        self._patch_space = None
+        self._unpatch_space = None
+        self._merge_space = None
+        self._split_space = None
+        self._attend_space = None
+
+    def compute_root_dim(self, input_dim: int) -> int:
+        return 2 ** int(0.5 * math.log2(input_dim))
+
+    def compute_output_shape(self, input_shape: tuple) -> tuple:
+        return tuple(input_shape)
+
+    def build(self, input_shape: tuple) -> None:
+        __batch_dim, __height_dim, __width_dim, __feature_dim = input_shape
+        # internal dimensions
+        __head_num = self._config['head_num'] or self.compute_root_dim(__feature_dim)
+        __patch_dim = self._config['patch_dim'] or min(self.compute_root_dim(__height_dim), self.compute_root_dim(__width_dim))
+        __space_dim = (__height_dim * __width_dim) // (__patch_dim ** 2)
+        __latent_dim = __patch_dim * __patch_dim * __feature_dim
+        __hidden_dim = 4 * __latent_dim
+        __head_dim = __latent_dim // __head_num
+        # init the layers
+        self._patch_space = mlable.blocks.shaping.PixelPacking(patch_dim=__patch_dim, height_axis=1, width_axis=2)
+        self._unpatch_space = mlable.blocks.shaping.PixelShuffle(patch_dim=__patch_dim, height_axis=1, width_axis=2)
+        self._merge_space = mlable.layers.shaping.Merge(axis=1, right=True)
+        self._split_space = mlable.layers.shaping.Divide(axis=1, factor=__width_dim // __patch_dim, insert=True, right=True)
+        self._attend_space = mlable.blocks.transformer.ResidualDecoderBlock(head_num=__head_num, key_dim=__head_dim, value_dim=__head_dim, hidden_dim=__hidden_dim, attention_axes=[1], epsilon=self._config['epsilon_rate'], dropout_rate=self._config['dropout_rate'], use_position=False, use_bias=True, center=True, scale=True)
+        # built with the specific shape, at each step
+        self._patch_space.build(input_shape)
+        self._merge_space.build((__batch_dim, __height_dim // __patch_dim, __width_dim // __patch_dim, __latent_dim))
+        self._attend_space.build(query_shape=(__batch_dim, __space_dim, __latent_dim), key_shape=(__batch_dim, __space_dim, __latent_dim), value_shape=(__batch_dim, __space_dim, __latent_dim))
+        self._split_space.build((__batch_dim, __space_dim, __latent_dim))
+        self._unpatch_space.build((__batch_dim, __height_dim // __patch_dim, __width_dim // __patch_dim, __latent_dim))
+        # register
+        self.built = True
+
+    def call(self, inputs: tf.Tensor, training: bool=False, **kwargs) -> tf.Tensor:
+        # global patching (B, H, W, E) => (B, H/P, W/P, PPE)
+        __outputs = self._patch_space(inputs)
+        # merge the patch and feature axes (B, H/P, W/P, PPE) => (B, HW/PP, PPE)
+        __outputs = self._merge_space(__outputs)
+        # space attention (across grids) (B, HW/PP, PPE)
+        __outputs = self._attend_space(query=__outputs, key=__outputs, value=__outputs, training=training, use_causal_mask=False)
+        # split patch features (B, HW/PP, PPE) => (B, H/P, W/P, PPE)
+        __outputs = self._split_space(__outputs)
+        # swap the axes back (B, H/P, W/P, PPE) => (B, H, W, E)
+        return self._unpatch_space(__outputs)
+
+    def get_config(self) -> dict:
+        __config = super(PatchTransformerBlock, self).get_config()
+        __config.update(self._config)
+        return __config
+
+    @classmethod
+    def from_config(cls, config) -> tf.keras.layers.Layer:
+        return cls(**config)
